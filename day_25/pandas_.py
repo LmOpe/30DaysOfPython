f7af527e353266d0b739a770f9be5310c1f701f4
LmOpe <lawalmuhammedope@gmail.com>
2023-12-12 23:09:16 +0100

    Added Day 25 Exercises

---
+++ day_25/pandas_.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df = pd.read_csv('./day_25/hacker_news.csv')
+
+print(df.head())
+
+print(df.tail())
+
+title = df['title']
+
+series = pd.Series(title)
+print(series)
+
+rows, columns = df.shape
+
+print(f"Rows: {rows} and Columns: {columns}")
+
+print(df[['python' in title for title in list(df['title'])]])
+
+
+print(df[['javascript' in title for title in list(df['title'])]])
